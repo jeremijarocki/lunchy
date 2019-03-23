1fe58cf2dd64e98aedd1fc2259b29735d66c59b4
Kamil Ossowski <k5x3s1@gmail.com>
2019-03-23 09:48:21 +0100

    Poprawiony template

--- src/main/java/sda/soft/academy/lunchyproject/lunchy/converters/OrderItemConverter.java
+++ src/main/java/sda/soft/academy/lunchyproject/lunchy/converters/OrderItemConverter.java
@@ -24,9 +24,9 @@ public class OrderItemConverter implements BiFunction<OrderItemDto, Order, Order
         orderItem.setOrder(order);
 
         //TODO rozwiąż ten problem, nie wiem z czego to wynika
-//        Long dishId = orderItemDto.getDishId();
-//        Optional<Dish> dish = dishRepository.findById(dishId);
-//        dish.isPresent(dish2 -> orderItem.setDish(dish2));
+        Long dishId = orderItemDto.getDishId();
+        Optional<Dish> dish = dishRepository.findById(dishId);
+        dish.ifPresent(dish2 -> orderItem.setDish(dish2));
 
         return orderItem;
     }
