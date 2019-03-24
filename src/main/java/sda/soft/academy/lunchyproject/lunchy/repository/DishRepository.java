60df539562c999c84807b6b70a45b73ce9fa602f
Kamil Ossowski <k5x3s1@gmail.com>
2019-03-24 12:52:42 +0100

    Duzo rzeczy, magia i inne takie

--- src/main/java/sda/soft/academy/lunchyproject/lunchy/repository/DishRepository.java
+++ src/main/java/sda/soft/academy/lunchyproject/lunchy/repository/DishRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import sda.soft.academy.lunchyproject.lunchy.entities.Dish;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,8 @@ public interface DishRepository extends CrudRepository<Dish, Long> {
 
     @Override
     Optional<Dish> findById(Long aLong);
+
+    Optional<List<Dish>> findAllByCatererId(Long catererId);
+
+    Optional<List<Dish>> findAllBy();
 }
